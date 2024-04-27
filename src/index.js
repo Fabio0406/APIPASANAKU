1c0190a6e0af7a332e72b00a78c5b99f4107e28e
Fabio Andre Arredondo Mendieta <fabioarredondo44@gmail.com>
2024-04-27 12:17:01 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -15,7 +15,10 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 const __dirname = dirname(fileURLToPath(import.meta.url))
 app.use(express.static(join(__dirname, 'public')))
-app.use(cors())
+app.use(cors(
+    origin= '*',
+    credentials= true,
+))
 
 
 
